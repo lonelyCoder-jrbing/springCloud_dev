02bcb30e8b3d2ad79daf2b21a30737cb03fc38d6
jurongbing <719203729@qq.com>
2019-10-31 00:15:08 +0800

    2019-10-31 jurongbing lambda test

---
+++ demoEureka/jdk8/src/main/java/com/example/jdk8/streamTest/lambdaTest/TestInterface/FunctionalInterfaceTest.java
@@ -0,0 +1,9 @@
+package com.example.jdk8.streamTest.lambdaTest.TestInterface;
+
+import com.example.jdk8.streamTest.Student;
+
+public interface FunctionalInterfaceTest {
+
+void getName(Student x);
+
+}
---
+++ demoEureka/jdk8/src/main/java/com/example/jdk8/streamTest/lambdaTest/impl/FunctionImpl.java
@@ -0,0 +1,14 @@
+package com.example.jdk8.streamTest.lambdaTest.impl;
+
+
+import com.example.jdk8.streamTest.Student;
+import com.example.jdk8.streamTest.lambdaTest.TestInterface.FunctionalInterfaceTest;
+
+public class FunctionImpl implements FunctionalInterfaceTest {
+
+
+    @Override
+    public void getName(Student x) {
+        System.out.println("hello world"+x);
+    }
+}
