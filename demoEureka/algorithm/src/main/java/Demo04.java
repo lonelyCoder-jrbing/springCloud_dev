273397fd2ad4acb330799eb99436b6c8e9496291
jurongbing <719203729@qq.com>
2019-11-17 08:45:48 +0800

    2019-11-17 jurongbing 算法

---
+++ demoEureka/algorithm/src/main/java/Demo04.java
@@ -0,0 +1,14 @@
+public class Demo04 {
+    public static void main(String[] args) {
+        String  s = "woshijurongbing";
+        Character c = (Character)s.charAt(3);
+        Character b = (Character)s.charAt(4);
+          if(c.equals(b)){
+
+
+
+          }
+
+
+    }
+}
